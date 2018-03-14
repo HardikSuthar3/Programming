aa1c3319db7d157963a82a5ed7d83e51bb82bf14
Hardik Suthar <hasuthar@microsoft.com>
2018-03-14 16:29:31 +0530

    Changes

--- main.cpp
+++ main.cpp
@@ -1,35 +1,7 @@
-#include "BinaryTree/BTree.h"
-
-
-long long C(int n, int r) {
-    if (r > n / 2) r = n - r; // because C(n, r) == C(n, n - r)
-    long long ans = 1;
-    int i;
-
-    for (i = 1; i <= r; i++) {
-        ans *= n - r + i;
-        ans /= i;
-    }
-
-    return ans;
-}
-
-int main() {
-    /* int t;
-     cin >> t;
-
-     t--;
-     int n, m;
-     cin >> n >> m;
-     MakeGraph(n, m);
-     PrintGraph();*/
-    BT T(1);
-    T.Add(1, 2, 3);
-    T.Add(2, 4, 5);
-
-    cout << T.Diameter(T.V[1]);
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
     return 0;
 }
-
-
-
